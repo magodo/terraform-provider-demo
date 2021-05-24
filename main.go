415707f6c01f7707aefa14bfc1dd039062e03386
magodo <wztdyl@sina.com>
2021-05-24 17:15:24 +0800

    debuggable (need to export `PLUGIN_PROTOCOL_VERSIONS` to 5)

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
 
 	"github.com/magodo/terraform-provider-demo/lib"
@@ -11,9 +13,26 @@ import (
 
 func main() {
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return lib.Provider()
-		},
-	})
+
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debuggable", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/demo",
+			&plugin.ServeOpts{
+				ProviderFunc: func() terraform.ResourceProvider {
+					return lib.Provider()
+				},
+			})
+		if err != nil {
+			log.Println(err.Error())
+		}
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: func() terraform.ResourceProvider {
+				return lib.Provider()
+			},
+		})
+	}
 }
